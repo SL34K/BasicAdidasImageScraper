8f7c252930b2dbfbed5f580843bef5c7c5b0833f
sleakk <30938710+sleakk@users.noreply.github.com>
2018-03-06 16:11:28 +0000

    Updated AdidasImageScraper.py
    Now pulls detailed images of the product aswell.

--- AdidasImageScraper.py
+++ AdidasImageScraper.py
@@ -2,7 +2,9 @@
 ##DM me on Twitter with questions.
 import requests, shutil
 url = 'https://www.adidas.co.uk/dis/dw/image/v2/aagl_prd/on/demandware.static/-/Sites-adidas-products/default/dw133db8e5/zoom/{}_{}_standard.jpg'
+durl = 'https://www.adidas.co.uk/dis/dw/image/v2/aagl_prd/on/demandware.static/-/Sites-adidas-products/default/dw133db8e5/zoom/{}_{}.jpg'
 def pull(pid):
+    print("Saving standard images...")
     #image 1
     url1 = url.format(pid,'01')
     response = requests.get(url1, stream=True)
@@ -51,7 +53,34 @@ def pull(pid):
         shutil.copyfileobj(response.raw, out_file)
     del response
     print("Successfully Saved {}_{}.png".format(pid,'06'))
+    print("Saving detailed images...")
+    #image detailed 1
+    url7 = durl.format(pid,'41_detail')
+    response = requests.get(url7, stream=True)
+    fname = pid+'_41_detail.jpg'
+    with open(fname, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+    print("Successfully Saved {}_{}.jpg".format(pid,'41_detail'))
+    #image detailed 2
+    url8 = durl.format(pid,'42_detail')
+    response = requests.get(url8, stream=True)
+    fname = pid+'_42_detail.jpg'
+    with open(fname, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+    print("Successfully Saved {}_{}.jpg".format(pid,'42_detail'))
+    #image detailed 3
+    url9 = durl.format(pid,'43_detail')
+    response = requests.get(url9, stream=True)
+    fname = pid+'_43_detail.jpg'
+    with open(fname, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+    print("Successfully Saved {}_{}.jpg".format(pid,'43_detail'))
+    
 
 print("@SL34k's Adidas Image Scraper")
 pid = input("Enter PID: ")
 pull(pid)
+input("Press enter to exit")
